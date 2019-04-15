22f995eea85b584d808a59159d2baa6859b119d0
Fernanda Guimaraes de Araujo <fernandaguim@madeira.grad.dcc.ufmg.br>
2019-04-15 11:47:07 -0300

    Added Observer Pattern

---
+++ ObserverPattern.java
@@ -0,0 +1,43 @@
+public interface Observer {
+  public void update(Subject s);
+}
+
+
+public class Subject {
+  private List observers = new LinkedList();
+
+
+  public void addObserver(Observer observer) {
+    observers.add(observer);
+  }
+
+  public void removeObserver(Observer observer) {
+    observers.remove(observer);
+  }
+  
+  public void notifyObservers() {
+    Iterator it = observers.iterator();
+    while (it.hasNext()) {
+      Observer obs= (Observer) it.next();
+      obs.update(this); 
+    }
+  }
+}
+
+public class Temperature extends Subject  {
+  private double value;
+  public double getValue() {
+    return value;
+  }
+  public void setValue(double value) {
+    This.value = value;
+    notifyObservers();
+  }
+}
+
+public class TermometerCelsius implements Observer  {
+  public void update(Subject s) {
+    double value = ((Temperature) s).getValue();
+    System.out.println("Celsius: " + value);
+  }
+}
